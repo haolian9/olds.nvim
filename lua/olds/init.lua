f2f292758adf27b650a211d83b6fb8a314d9908a
haoliang <haoliang0.1.2@gmail.com>
2023-03-14 16:25:26 +0800

    finish the lua part

--- lua/olds/init.lua
+++ lua/olds/init.lua
@@ -1,1 +1,71 @@
+local M = {}
 
+local fs = require("infra.fs")
+local jelly = require("infra.jellyfish")("olds")
+
+local redis = require("olds.redis")
+local unsafe = require("olds.unsafe")
+
+local api = vim.api
+local uv = vim.loop
+
+local facts = {
+  aug = api.nvim_create_augroup("olds", {}),
+  global_zset = string.format("%s:nvim:olds:global", uv.getuid()),
+}
+
+local state = {
+  has_setup = false,
+  history = {},
+}
+
+---@param bufnr number
+---@return string?
+local function resolve_fpath(bufnr)
+  -- is a 'regular file' buffer
+  if api.nvim_buf_get_option(bufnr, "buftype") ~= "" then return end
+  local bufname = api.nvim_buf_get_name(bufnr)
+  -- has been named
+  if bufname == "" then return end
+  if fs.is_absolute(bufname) then return bufname end
+  return vim.fn.expand("%:p", bufname)
+end
+
+function M.setup(sock_path)
+  if state.has_setup then return end
+
+  if not redis.connect_unix(sock_path) then return jelly.err("unable connect to redis") end
+  state.has_setup = true
+
+  -- learnt these timings from https://github.com/ii14/dotfiles/blob/master/.config/nvim/lua/mru.lua
+  api.nvim_create_autocmd({ "bufenter", "bufwritepost" }, {
+    group = facts.aug,
+    callback = function(args)
+      local bufnr = args.buf
+      local path = resolve_fpath(bufnr)
+      if path == nil then return end
+      local time = os.time()
+      table.insert(state.history, time)
+      table.insert(state.history, path)
+    end,
+  })
+  api.nvim_create_autocmd({ "focuslost", "vimsuspend", "vimleavepre" }, {
+    group = facts.aug,
+    callback = function()
+      local hist = state.history
+      if #hist == 0 then return end
+      state.history = {}
+      redis.zadd(facts.global_zset, unpack(hist))
+    end,
+  })
+  api.nvim_create_autocmd("vimleave", {
+    group = facts.aug,
+    callback = function()
+      assert(state.has_setup)
+      assert(#state.history == 0)
+      redis.close()
+    end,
+  })
+end
+
+return M
