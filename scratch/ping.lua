1edb0e1217d71939424b3637467f04a1fcea579f
haoliang <haoliang0.1.2@gmail.com>
2023-08-23 06:53:05 +0800

    prune scratch files

--- scratch/ping.lua
+++
@@ -1,6 +0,0 @@
-local RedisClient = require("olds.RedisClient")
-
-local client = RedisClient.connect_unix("/run/user/1000/redis.sock")
-
-local reply = client:send("ping")
-print(reply.data, reply.err)
