8d94563bcd84d07bdd12855a034b6af85c08c45e
haoliang <haoliang0.1.2@gmail.com>
2024-06-06 17:10:44 +0800

    sync with upstream

--- lua/olds/protocol/pack.lua
+++ lua/olds/protocol/pack.lua
@@ -1,21 +1,24 @@
+local ropes = require("string.buffer")
+
 --A client sends the Redis server a RESP Array consisting of only Bulk Strings.
 ---@param cmd string
 ---@param ... string|number
 ---@return string
 return function(cmd, ...)
   assert(cmd)
-  local parts
-  do
-    local nargs = select("#", ...)
-    local total = 1 + nargs
-    parts = { "*" .. total, "$" .. #cmd, cmd }
-    for i = 1, nargs do
-      local a = select(i, ...)
-      if type(a) ~= "string" then a = tostring(a) end
-      table.insert(parts, "$" .. #a)
-      table.insert(parts, a)
-    end
-    table.insert(parts, "")
+
+  local nargs = select("#", ...)
+  local total = 1 + nargs
+  local rope = ropes.new()
+
+  rope:putf("*%d\r\n", total)
+  rope:putf("$%d\r\n", #cmd):putf("%s\r\n", cmd)
+
+  for i = 1, nargs do
+    local a = select(i, ...)
+    if type(a) ~= "string" then a = tostring(a) end
+    rope:putf("$%d\r\n", #a):putf("%s\r\n", a)
   end
-  return table.concat(parts, "\r\n")
+
+  return rope:get()
 end
