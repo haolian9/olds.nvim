1aef3b62de23dc053a4ecb1aba1c714454bb5643
haoliang <haoliang0.1.2@gmail.com>
2024-06-23 10:23:24 +0800

    sync with upstream

--- lua/olds/protocol/pack.lua
+++ lua/olds/protocol/pack.lua
@@ -1,5 +1,7 @@
 local ropes = require("string.buffer")
 
+local rope = ropes.new()
+
 --A client sends the Redis server a RESP Array consisting of only Bulk Strings.
 ---@param cmd string
 ---@param ... string|number
@@ -9,7 +11,6 @@ return function(cmd, ...)
 
   local nargs = select("#", ...)
   local total = 1 + nargs
-  local rope = ropes.new()
 
   rope:putf("*%d\r\n", total)
   rope:putf("$%d\r\n", #cmd):putf("%s\r\n", cmd)
