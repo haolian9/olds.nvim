73d31a24022ccecddaa423add712c3a5f8d62ba5
haoliang <haoliang0.1.2@gmail.com>
2026-01-01 16:45:56 +0800

    sync with upstream

--- lua/olds/protocol/init.lua
+++ lua/olds/protocol/init.lua
@@ -1,7 +1,9 @@
 --ref: https://github.com/redis/redis-specifications/blob/master/protocol/RESP2.md
 
+local oop = require("infra.oop")
+
 return {
-  pack = require("olds.protocol.pack"),
-  unpack = require("olds.protocol.unpack"),
-  Stash = require("olds.protocol.Stash"),
+  pack = oop.proxy("olds.protocol.pack"),
+  unpack = oop.proxy("olds.protocol.unpack"),
+  Stash = oop.proxy("olds.protocol.Stash"),
 }
